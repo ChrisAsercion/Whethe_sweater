35b74a79cb928a0f35d7cb14e4e93617040edbb0
Chris Asercion <chris.asercion@gmail.com>
2023-09-25 11:29:26 -0600

    Adds facade spec

---
+++ spec/facade/books_facade_spec.rb
@@ -0,0 +1,26 @@
+
+require 'rails_helper'
+
+describe BooksFacade do
+  let(:query) { 'Ruby Programming' }
+  let(:quantity) { 10 }
+  
+  describe '#get_data' do
+    it 'calls BooksService with the provided query and quantity' do
+
+      books_facade = BooksFacade.new(query, quantity)
+
+      json = books_facade.get_data
+
+
+      expect(json[:numFound]).to be_a Integer
+      expect(json[:docs]).to be_a Array
+      json[:docs].each do |book|
+        expect(book[:isbn]).to be_a(Array).or be_nil
+        
+        expect(book).to have_key(:title)
+        expect(book[:title]).to be_a(String)
+      end
+    end
+  end
+end
